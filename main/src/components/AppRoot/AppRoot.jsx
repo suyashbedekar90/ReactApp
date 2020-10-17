157348ceb84ade711ff59adfc6270c6272f15eca
SuyashBedekar <suyash.bedekar@target.com>
2020-10-17 12:12:45 -0500

    added search component and cleanup

---
+++ main/src/components/AppRoot/AppRoot.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Search from '../Search/Search';
+
+const AppRoot = () => {
+    return (
+        <div>
+            <Search />
+        </div>
+    );
+}
+
+export default AppRoot;
